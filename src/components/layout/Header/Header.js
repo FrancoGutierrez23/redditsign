fe9432b8013562dd484feff1f564cd99b1736af0
Franco Gutierrez <francogutierre523523@gmail.com>
2024-06-26 18:27:24 -0300

    Improves styles, mainly the Header's styles, including the font-family, logo added, change the color scheme

--- src/components/layout/Header/Header.js
+++ src/components/layout/Header/Header.js
@@ -3,9 +3,11 @@ import './Header.css'
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { fetchSearchResults } from '../../../redux/postsSlice';
+import logo from '../../../assets/logo_64px.jpeg'; // Import the logo image
 
 const Header = () => {
   const [searchQuery, setSearchQuery] = useState('');
+  const [isSearchActive, setIsSearchActive] = useState(false);
   const dispatch = useDispatch();
 
   const handleSearchSubmit = (e) => {
@@ -15,18 +17,22 @@ const Header = () => {
     }
   };
 
+  const handleSearchButtonClick = () => {
+    setIsSearchActive(true);
+  };
+
   return (
     <header>
-      <h1>Redditsign</h1>
-      <form onSubmit={handleSearchSubmit} name='search' className='search_form'>
+      <div className='logo'><img alt='logo' src={logo} style={{width: '64px', height: 'auto'}} /><h1>Redditsign</h1></div> {/* Use the imported logo */}
+      <form onSubmit={handleSearchSubmit} name='search' className='search_form search-bar'>
         <input 
           type="text" 
-          className='search_box'
+          className={`search_box ${isSearchActive ? 'active' : ''}`}
           placeholder="Search posts..." 
           value={searchQuery} 
           onChange={(e) => setSearchQuery(e.target.value)} 
         />
-        <button type="submit" className='search_button'>Search</button>
+        <button type="button" className='search_button btn-search' onClick={handleSearchButtonClick}>Search</button>
       </form>
     </header>
   );
