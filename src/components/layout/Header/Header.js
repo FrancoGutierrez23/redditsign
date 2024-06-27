bd2a70f3e2f2de723d2fe581e19e0f71553da33d
Franco Gutierrez <francogutierre523523@gmail.com>
2024-06-27 09:18:17 -0300

    Add final styles to the header

--- src/components/layout/Header/Header.js
+++ src/components/layout/Header/Header.js
@@ -1,14 +1,16 @@
 //src/components/layout/Header/Header.js
 import './Header.css'
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { useDispatch } from 'react-redux';
 import { fetchSearchResults } from '../../../redux/postsSlice';
 import logo from '../../../assets/logo_64px.jpeg'; // Import the logo image
+import searchIcon from '../../../assets/search.png'
 
 const Header = () => {
   const [searchQuery, setSearchQuery] = useState('');
   const [isSearchActive, setIsSearchActive] = useState(false);
   const dispatch = useDispatch();
+  const searchBoxRef = useRef(null);
 
   const handleSearchSubmit = (e) => {
     e.preventDefault();
@@ -21,18 +23,38 @@ const Header = () => {
     setIsSearchActive(true);
   };
 
+  const handleLogoClick = () => {
+    window.location.reload(); // Reload the page when logo is clicked
+  };
+
+  // Add event listener to handle click outside search box
+  useEffect(() => {
+    const handleClickOutside = (e) => {
+        if (searchBoxRef.current && !searchBoxRef.current.contains(e.target)) {
+            setIsSearchActive(false);
+        }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+    
+    return () => {
+        document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [searchBoxRef]);
+
   return (
     <header>
-      <div className='logo'><img alt='logo' src={logo} style={{width: '64px', height: 'auto'}} /><h1>Redditsign</h1></div> {/* Use the imported logo */}
+      <div className='logo' onClick={handleLogoClick}><img alt='logo' src={logo} style={{width: '64px', height: 'auto'}} /><h1>Redditsign</h1></div> {/* Use the imported logo */}
       <form onSubmit={handleSearchSubmit} name='search' className='search_form search-bar'>
+        <button type="button" className='search_button btn-search' onClick={handleSearchButtonClick}><img alt='search' src={searchIcon} className='search_icon' /></button>
         <input 
           type="text" 
           className={`search_box ${isSearchActive ? 'active' : ''}`}
           placeholder="Search posts..." 
           value={searchQuery} 
           onChange={(e) => setSearchQuery(e.target.value)} 
+          ref={searchBoxRef}
         />
-        <button type="button" className='search_button btn-search' onClick={handleSearchButtonClick}>Search</button>
       </form>
     </header>
   );
