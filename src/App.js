ac2c12e6af5d11a8b571eb5915788b0e4b5138d9
Franco Gutierrez <francogutierre523523@gmail.com>
2024-07-04 08:20:17 -0300

    Fix issue with  Cookies with the SameSite=None for chromium based browsers

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ const App = () => {
   const error = useSelector((state) => state.posts.error);
   const after = useSelector((state) => state.posts.after);
   const selectedSubreddit = useSelector((state) => state.posts.selectedSubreddit);
+  document.cookie = "key=value; SameSite=None; Secure";
 
   return (
     <div className="App">
