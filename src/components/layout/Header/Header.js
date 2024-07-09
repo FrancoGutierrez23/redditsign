8cdebebb143cc2e56e5badeb259eda07c00072e5
Franco Gutierrez <francogutierre523523@gmail.com>
2024-07-09 18:08:28 -0300

    Add Header component test

--- src/components/layout/Header/Header.js
+++ src/components/layout/Header/Header.js
@@ -7,10 +7,6 @@ import logo from '../../../assets/logo_64px.jpeg'; // Import the logo image
 import searchIcon from '../../../assets/search.png';
 import SubredditList from '../Subreddits/SubredditList';
 
-
-// better handling the loading and error states
-// handling videos and gif formats
-
 const Header = () => {
   const [searchQuery, setSearchQuery] = useState('');
   const [isSearchActive, setIsSearchActive] = useState(false);
--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,11 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+// Mocking global functions and properties
+window.scrollTo = jest.fn();
+Object.defineProperty(window, 'location', {
+  value: {
+    ...window.location,
+    reload: jest.fn()
+  },
+  writable: true
+});
