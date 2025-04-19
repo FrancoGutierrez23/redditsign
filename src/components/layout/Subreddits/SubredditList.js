f63d7f0ac0063821f311b94fcba2c8e58a39fdba
Franco Gutierrez <francogutierre523523@gmail.com>
2025-04-19 09:11:31 -0300

    Improves responsiveness of subredditList.

--- src/components/layout/Subreddits/SubredditList.js
+++ src/components/layout/Subreddits/SubredditList.js
@@ -2,12 +2,22 @@ import React, { useEffect, useState, useRef } from "react";
 import { useDispatch } from "react-redux";
 import { fetchPosts, resetPosts } from "../../../redux/postsSlice";
 import "./SubredditList.css";
+import { MdOutlineMenuOpen } from "react-icons/md";
 
 const SubredditList = () => {
   const dispatch = useDispatch();
   const [selectedSubreddit, setSelectedSubreddit] = useState("pics");
   const [isListVisible, setIsListVisible] = useState(false);
   const listRef = useRef(null);
+  const [width, setWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => setWidth(window.innerWidth);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  console.log(width);
 
   useEffect(() => {
     dispatch(resetPosts()); // Reset posts when subreddit changes
@@ -49,9 +59,20 @@ const SubredditList = () => {
 
   return (
     <aside className="subreddits_list" ref={listRef}>
-      <h3 className="subreddits_title" onClick={toggleListVisibility}>
-        Subreddits
-      </h3>
+      {width > 600 ? (
+        <div className="menu" onClick={toggleListVisibility}>
+        <MdOutlineMenuOpen className="menu_logo" />
+      
+        <h3 className="subreddits_title" onClick={toggleListVisibility}>
+          Subreddits
+        </h3>
+        </div>
+      ) : (
+        <div className="menu" onClick={toggleListVisibility}>
+          <MdOutlineMenuOpen className="menu_logo" />
+        </div>
+      )}
+
       {isListVisible && (
         <div className="subreddits_options_container">
           {[
